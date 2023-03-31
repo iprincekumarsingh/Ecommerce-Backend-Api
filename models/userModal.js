4f09c701a5a09bf880850af7d5b4325807f94078
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-03-31 08:42:20 +0530

    Created User Modal

---
+++ models/userModal.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const { Schema } = mongoose;
+
+const UserScheama = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    phone: {
+        type: String,
+        required: false
+    },
+    avatar: {
+        type: String
+    },
+
+
+
+}, { timestamps: true })
+
+
+UserScheama.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.generateAuthToken = async function () {
+    const token = await jwt.sign({ _id: this._id }, process.env.JWT_SECERT);
+    return token;
+
+}
+
+userSchema.methods.isVerifiedpassword = async function (password) {
+    return await bcrypt.compare(password,this.password)
+
+}
+
+const User = mongoose.model('User', UserScheama);
+
+module.exports = User;
